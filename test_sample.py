c335dee2f4563a69cc5da3749e9b913353a06660
cristianobeltranjosephsebastia-design <cristianobeltranjosephsebastia@gmail.com>
2026-08-25 09:59:39 -0500

    EliminandoPruebaFallida-EvidenciandoDesplegue

--- test_sample.py
+++ test_sample.py
@@ -2,10 +2,13 @@ import pytest
 import sys
 import os
 
-from backend.sample_app import sample
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'backend')))
 
+from backend.sample_app import sample
+
+def test_ejemplo_basico():
+    assert 1 + 1 == 2 
+    
 @pytest.fixture
 def client():
     sample.config['TESTING'] = True
@@ -16,9 +19,4 @@ def test_home_status_code(client):
     response = client.get('/')
     assert response.status_code == 200
 
-def test_ejemplo_basico():
-    assert 1 + 1 == 2 
-
-def test_ejemplo_fallido():
-    assert 1 + 1 == 3
 
